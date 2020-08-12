3f3ab754c32200cce00549c05e3cd731f54d0f10
fabiomorais <fabio.jorge@gmail.com>
2020-08-12 16:10:34 -0300

    Reestruturação do repositório

--- app/infoEquipe/infoEquipe.js
+++
@@ -1,9 +0,0 @@
-
-app.directive('infoEquipe', function(){
-    return {
-        restrict : 'E',
-        scope: {equipe: '='},
-        templateUrl: 'app/infoEquipe/infoEquipe.html',
-        css: 'app/infoEquipe/infoEquipe.css'
-    }
-})
