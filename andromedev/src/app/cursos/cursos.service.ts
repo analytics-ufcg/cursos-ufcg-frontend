0f55c00a76c9f21afe4940ec436582f99a0c7f42
albertosilv <jose.alberto.silva@ccc.ufcg.edu.br>
2020-09-23 14:30:49 -0300

    getCursos

--- andromedev/src/app/cursos/cursos.service.ts
+++ andromedev/src/app/cursos/cursos.service.ts
@@ -1,9 +1,16 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Curso } from '../interface/cursos.interface'
+import {Observable} from 'rxjs'
+import{Injectable} from '@angular/core'
+import {Disciplina} from '../interface/disciplina.interface'
 
 @Injectable({
   providedIn: 'root'
 })
 export class CursosService {
-
-  constructor() { }
+  private url='http://analytics.ufcg.edu.br/pre/'
+  constructor(private http: HttpClient){}
+  getCursos():Observable<Curso[]>{
+      return this.http.get<Curso[]>(`${this.url}cursos_2015`)
+  }
 }
